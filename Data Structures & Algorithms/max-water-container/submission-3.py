ab6cf6bec9a653caf06bfc17c848872acfee34f6
Jay Chen <145420661+jayeee66@users.noreply.github.com>
2026-04-11 17:02:18 +0800

    Update submission-3.py

--- Data Structures & Algorithms/max-water-container/submission-3.py
+++ Data Structures & Algorithms/max-water-container/submission-3.py
@@ -7,6 +7,11 @@ class Solution:
             area = (min(heights[l], heights[r])) * (r - l)
             if area > res:
                 res = area
+                
+            # To find a larger area. Since moving either pointer 
+            # decreases the width (r - l), we must try to increase the height.
+            # Therefore, we always move the pointer pointing to the shorter bar,
+            # because the shorter bar is the "bottleneck" that limits the area.
             if heights[l] <= heights[r]:
                 l += 1
             elif heights[l] > heights[r]:
