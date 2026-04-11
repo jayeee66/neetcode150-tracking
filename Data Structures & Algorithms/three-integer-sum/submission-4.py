5a3ef708523b0fee4f5a3f3abcd28de7a481b186
Jay Chen <145420661+jayeee66@users.noreply.github.com>
2026-04-11 13:36:09 +0800

    Update submission-4.py

--- Data Structures & Algorithms/three-integer-sum/submission-4.py
+++ Data Structures & Algorithms/three-integer-sum/submission-4.py
@@ -1,20 +1,21 @@
-#Two pointer
+# Two Pointer
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         res = []
         for i in range(len(nums) - 2):
-            # Prevent two duplicate number 
+            # Prevent two duplicate numbers
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             l = i + 1
             r = len(nums) - 1
-            
+            # To prevent left index passing right index, it's important
             while l < r:
                 three_sum = nums[i] + nums[l] + nums[r]
                 if three_sum == 0:
                     res.append([nums[i], nums[l], nums[r]])
                     l += 1
+                    # Make sure the left index won't count again
                     while l < r and nums[l] == nums[l-1]:
                         l += 1
                 elif three_sum > 0:
