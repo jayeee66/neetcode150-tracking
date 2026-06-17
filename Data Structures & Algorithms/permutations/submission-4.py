6ea89d57376e34c6113f6f037b0ebe0de2c676bf
Jay Chen <145420661+jayeee66@users.noreply.github.com>
2026-06-17 12:33:45 +0800

    Add: permutations - submission-4

---
+++ Data Structures & Algorithms/permutations/submission-4.py
@@ -0,0 +1,23 @@
+#backtracking
+#optimal
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+        #record condition of existing in subset list
+        used = [False] * len(nums)
+        def dfs():
+            if len(subset) == len(nums):
+                res.append(subset.copy())
+                return
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                subset.append(nums[i])
+                used[i] = True
+                dfs()
+                subset.pop()
+                used[i] = False
+        dfs()
+        return res
+            
